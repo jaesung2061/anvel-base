9ba2e46f927e12073abd8e631f509dda49800313
Jeff Yeon <jeffyeon2061@gmail.com>
2015-06-18 20:30:19 -0700

    changes to env example

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
 	|
 	*/
 
-	'key'             => env('APP_KEY', env('APP_KEY', 'SomeRandomString')),
+	'key'             => env('APP_KEY', 'SomeRandomString'),
 
 	'cipher'          => MCRYPT_RIJNDAEL_128,
 
