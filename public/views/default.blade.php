e02f197ec09b07247157b755d045d151bde5e206
Jeff Yeon <jeffyeon2061@gmail.com>
2015-09-13 16:08:41 -0700

    Fixed wrong jQuery version (CDN script)

--- public/views/default.blade.php
+++ public/views/default.blade.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/vendor.css">
         <link rel="stylesheet" href="assets/main.css">
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.6/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular.js"></script>
         <script src="assets/vendor.js"></script>
         <script src="assets/main.js"></script>
